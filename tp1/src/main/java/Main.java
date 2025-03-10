23bacbde446eb35c0430150f046fcc971f933446
Gaston Alasia <galasia@itba.edu.ar>
2025-03-10 12:12:43 -0300

    feat: bidirectional mapping

--- tp1/src/main/java/Main.java
+++ tp1/src/main/java/Main.java
@@ -2,10 +2,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
@@ -18,7 +15,7 @@ public class Main {
     }
 
     static List<Cell> getNeighbourCells(Cell cell) {
-        return List.of(new Cell(cell.getX(), cell.getY()-1), new Cell(cell.getX()+1, cell.getY()-1), new Cell(cell.getX()+1, cell.getY()), new Cell(cell.getX()+1, cell.getY()+1));
+        return new ArrayList<>(List.of(new Cell(cell.getX(), cell.getY()-1), new Cell(cell.getX()+1, cell.getY()-1), new Cell(cell.getX()+1, cell.getY()), new Cell(cell.getX()+1, cell.getY()+1)));
     }
 
     public static void main(String[] args) {
@@ -60,7 +57,6 @@ public class Main {
             return;
         }
 
-        //Calculate which particles go to each cell
         particles.forEach(particle -> {
             Cell cell = getParticleCell(particle);
             List<Particle> list = particlesByCell.get(cell);
@@ -72,39 +68,38 @@ public class Main {
         }); {
         }
 
-        // 1 2 3
-        // 4 5 6
-        // 7 8 9
-
-        Map<Integer, List<Integer>> nearParticles = new HashMap<>();
+        Map<Integer, Set<Integer>> nearParticles = new HashMap<>();
+        particles.forEach(p -> nearParticles.put(p.getId(), new HashSet<>()));
 
         particles.forEach(particle -> {
             Cell cell = getParticleCell(particle);
             List<Cell> neighbourCells = getNeighbourCells(cell);
-            List<Particle> allNeighbours = new ArrayList<>();
+            neighbourCells.add(cell);
             neighbourCells.forEach(n -> {
-                List<Particle> nearParticlesList = particlesByCell.getOrDefault(n, new ArrayList<>()).stream().filter(p -> p.distanceTo(particle) <= Constants.getRc()).toList();
-                allNeighbours.addAll(nearParticlesList);
-            });
-            nearParticles.put(particle.getId(), allNeighbours.stream().map(Particle::getId).toList());
-        });
+                List<Particle> nearParticlesList = particlesByCell.getOrDefault(n, new ArrayList<>());
 
+                for (Particle neighbor : nearParticlesList) {
+                    if (particle.getId() != neighbor.getId() && particle.distanceTo(neighbor) <= Constants.getRc()) {
+                        nearParticles.get(particle.getId()).add(neighbor.getId());
+                        nearParticles.get(neighbor.getId()).add(particle.getId());
+                    }
+                }
+            });
+        });
 
         try (FileWriter fileWriter = new FileWriter("output_" + Constants.getN() + "_" + "rc" + Constants.getRc())) {
             nearParticles.forEach((key, value) ->
             {
                 try {
                     fileWriter.write(String.format("[%d: %s]\n", key, value.stream()
-                            .map(String::valueOf)  // Convert each Integer to a String
+                            .map(String::valueOf)
                             .collect(Collectors.joining(" "))));
                 } catch (IOException e) {
                     throw new RuntimeException(e);
                 }
-            });  // Join with a space separator
+            });
         } catch (IOException e) {
             System.out.println("Error writing to file: " + e.getMessage());
         }
     }
-
 }
-
